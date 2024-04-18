cc5e8a0e78026e4f42774bda2d6321711249d7e7
Taapke <64285579+Taapke@users.noreply.github.com>
2024-04-18 12:14:47 +0200

    Add home screen and toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserModule} from "@angular/platform-browser";
+import {HomeComponent} from "./components/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HttpClientModule],
+  imports: [RouterOutlet, RouterLink, HttpClientModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
